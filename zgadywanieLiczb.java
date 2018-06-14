b33af9bb869c2c0d1161561da56e7a5c5b4f7439
Arkadiusz1988 <36170683+Arkadiusz1988@users.noreply.github.com>
2018-06-14 13:44:21 +0200

    Add files via upload

--- zgadywanieLiczb.java
+++ zgadywanieLiczb.java
@@ -1,9 +1,50 @@
 
+import java.io.IOException;
+import java.util.Random;
+import java.util.Scanner;
+
+
 public class zgadywanieLiczb {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		Scanner	scan = new	Scanner(System.in);
+		boolean wynik = false;
+		Random rand = new Random();
+		int liczba = rand.nextInt(100);
 
+		while(wynik==false)
+		{
+			System.out.print("Zgadnij liczbę ");
+			String zgadnij = scan.next();
+
+			if (isInt(zgadnij)==true) {		    		    
+				if(Integer.parseInt(zgadnij)==liczba)
+				{
+					System.out.println("Znalazłeś szukaną liczbę!!!");
+					wynik=true;
+				} 
+				else if (Integer.parseInt(zgadnij)>liczba)
+				{
+					System.out.println("Za dużo!");
+					wynik=false;
+				} 
+				else if (Integer.parseInt(zgadnij)<liczba)
+				{
+					System.out.println("Za mało!");
+					wynik=false;
+				} 
+			} else {
+				System.out.println("To nie jest liczba.");
+			}
+		}		
 	}
 
+	public static boolean isInt(String str) {
+		try {
+			Integer.parseInt(str);
+		} catch (NumberFormatException e) {
+			return false;
+		}
+		return true;
+	}
 }
